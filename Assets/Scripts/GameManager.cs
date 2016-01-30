0e0f0bcace23a6e8cad3e2cc45190d4f6744407f
Goo <kuwoods@hotmail.com>
2016-01-30 15:56:18 +0800

    increase stage init and pass/fail handle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,32 @@
 using System.Collections;
 
 public partial class GameManager : Singleton<GameManager> {
-	private float passRequestTime = 3;
+	private int passRequestTime = 3;
+	private float punishTime = 1;
+
+	private float remainTimeRate { get { return remainTime / passTime; } }
+
+	private GameStatus gamestatus = GameStatus.NON;
+
 
 	protected override void Awake()
 	{
 		base.Awake();
 	}
 
-	private void Start() { }
+	private void Start() {
+		InitStage(0);
+		gamestatus = GameStatus.PROCESS;//gooku: tmp for test. will update to button control
+    }
 
 	private void Update() {
-		GameTimeHandle();
+
+		if(gamestatus == GameStatus.PROCESS)
+			GameTimeHandle();
 	}
 
 	public void GetDecide(bool _isPass) {
 		//TODO:	pass/fail handle
-
 		if (_isPass)
 		{
 			passRequestTime--;
@@ -26,7 +36,14 @@ public partial class GameManager : Singleton<GameManager> {
 				Pass();
 		}
 		else {
+			remainTime -= punishTime;
 
+			if (remainTime <= 0) {
+				gameTimeBar.UpdateBar(0);
+				GameOver();
+			} else {
+				gameTimeBar.UpdateBar(remainTimeRate);
+			}
 		}
 	}
 
@@ -39,9 +56,14 @@ public partial class GameManager : Singleton<GameManager> {
 	}
 
 	private void InitStage(int _index) {
-
-	}
+		remainTime = passTime;
+		passRequestTime = 3;
+		gameTimeBar.UpdateBar(remainTimeRate);
+		gameTimeBar.UpdateTime(passRequestTime);
+    }
 
 	private void UpdateStage(int _index) {
 	}
 }
+
+public enum GameStatus { NON, PROCESS, PASS, FAIL }
