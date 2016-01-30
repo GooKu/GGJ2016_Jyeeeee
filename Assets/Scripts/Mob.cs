5f2747f6df007fa6d1b21395207288a788b6b60f
ianchengtw <chang.baoh@gmail.com>
2016-01-30 22:14:43 +0800

    Add mob animation

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -2,84 +2,34 @@
 using System.Collections;
 
 public class Mob : MonoBehaviour {
-
-	public enum MoveDirection
-	{
-		right,
-		left,
-		top,
-		bottom
-	};
-
-	private float m_original_pos_x = 0.0f; 
-	private float m_original_pos_y = 0.0f; 
-	private MoveDirection m_move_direction;
-	private float m_move_speed_x = 0.0f; 
-	private float m_move_speed_y = 0.0f; 
-	private float m_movable_distance_x = 0.0f; 
-	private float m_movable_distance_y = 0.0f; 
-
+	
 	public Behavior m_behavior;
+
+	Animator m_anim;
 
 	// Use this for initialization
 	void Start () {
 		init();
 
-		m_original_pos_x = transform.position.x;
-		m_original_pos_y = transform.position.y;
+		m_anim = GetComponent<Animator> ();
 	}
 
 	void init () {
-		m_move_speed_x = 0.05f;
-		m_move_speed_y = 0.0f;
-		m_movable_distance_x = 4.0f;
-		m_movable_distance_y = 0.0f;
-		m_move_direction = MoveDirection.left;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		Move ();
-	}
-
-	void Move() {
-
-		switch (m_move_direction) {
-
-		case MoveDirection.right:
-			
-			transform.position = new Vector3 (transform.position.x + m_move_speed_x, transform.position.y);
-
-			if (transform.position.x > (m_original_pos_x + m_movable_distance_x)) {
-				m_move_direction = MoveDirection.left;
-			}
-
-			break;
-		case MoveDirection.left:
-			
-			transform.position = new Vector3(transform.position.x - m_move_speed_x, transform.position.y);
-
-			if (transform.position.x < (m_original_pos_x - m_movable_distance_x)) {
-				m_move_direction = MoveDirection.right;
-			}
-
-			break;
-		}
-
-
-		ShakeBody ();
-	}
-	void ShakeBody() {
-		
 	}
 
 	public void OnMouseHit () {
 		
 		switch (GetBehavior()) {
 		case Behavior.none:
+			break;
+		case Behavior.collect_behavior:
 			GameManager.Instance.GetDecide(false);
 			break;
-		case Behavior.noise:
+		case Behavior.wrong_behavior_noise:
 			//TODO:	minus player's life
 			MakeMobAsRegularMob();
 			GameManager.Instance.GetDecide(true);
@@ -96,6 +46,7 @@ public class Mob : MonoBehaviour {
 	}
 
 	private void MakeMobAsRegularMob() {
+		m_anim.SetBool ("alive", false);
 		SetBehavior(Behavior.none);
 	}
 
@@ -104,5 +55,6 @@ public class Mob : MonoBehaviour {
 public enum Behavior
 {
 	none,
-	noise
+	collect_behavior,
+	wrong_behavior_noise
 }
