1cae4ea9a3372935fb82370877123fa4928bf67b
ianchengtw <chang.baoh@gmail.com>
2016-01-30 16:59:48 +0800

    Mob call GameManager API when it got mouse hit

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -9,19 +9,74 @@ public class Mob : MonoBehaviour {
 		noise
 	};
 
+	public enum MoveDirection
+	{
+		right,
+		left,
+		top,
+		bottom
+	};
+
+	private float m_original_pos_x = 0.0f; 
+	private float m_original_pos_y = 0.0f; 
+	private MoveDirection m_move_direction;
+	private float m_move_speed_x = 0.0f; 
+	private float m_move_speed_y = 0.0f; 
+	private float m_movable_distance_x = 0.0f; 
+	private float m_movable_distance_y = 0.0f; 
+
 	private Behavior m_behavior;
 
 	// Use this for initialization
 	void Start () {
-		m_behavior = Behavior.none;
+		init();
+
+		m_original_pos_x = transform.position.x;
+		m_original_pos_y = transform.position.y;
 	}
 
 	void init () {
+		m_move_speed_x = 0.05f;
+		m_move_speed_y = 0.0f;
+		m_movable_distance_x = 4.0f;
+		m_movable_distance_y = 0.0f;
+		m_behavior = Behavior.noise;
+		m_move_direction = MoveDirection.left;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+//		Move ();
+	}
+
+	void Move() {
+
+		switch (m_move_direction) {
+
+		case MoveDirection.right:
+			
+			transform.position = new Vector3 (transform.position.x + m_move_speed_x, transform.position.y);
+
+			if (transform.position.x > (m_original_pos_x + m_movable_distance_x)) {
+				m_move_direction = MoveDirection.left;
+			}
+
+			break;
+		case MoveDirection.left:
+			
+			transform.position = new Vector3(transform.position.x - m_move_speed_x, transform.position.y);
+
+			if (transform.position.x < (m_original_pos_x - m_movable_distance_x)) {
+				m_move_direction = MoveDirection.right;
+			}
+
+			break;
+		}
+
+
+		ShakeBody ();
+	}
+	void ShakeBody() {
 	}
 
 	public void OnMouseHit () {
@@ -31,6 +86,7 @@ public class Mob : MonoBehaviour {
 			break;
 		case Mob.Behavior.noise:
 			//TODO:	minus player's life
+			GameManager.Instance.GetDecide(true);
 			break;
 		}
 
