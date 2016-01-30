003fa3610ebb2ca88231319c8292cf1505d1b423
Goo <kuwoods@hotmail.com>
2016-01-30 12:46:47 +0800

    increase GameManager logic struct and public API

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,30 @@
 using System.Collections;
 
 public class GameManager : Singleton<GameManager> {
+
+	private int passTime = 0;
+	private float gameTime;
+
 	protected override void Awake()
 	{
 		base.Awake();
 
+	}
+
+	private void Start() { }
+
+	private void Update() {
+		//TODO:reduce time
+	}
+
+	public void GetDecide(bool _resullt) {
+		//TODO:	pass/fail handle
+	}
+
+	private void InitStage(int _index) {
+
+	}
+
+	private void UpdateStage(int _index) {
 	}
 }
