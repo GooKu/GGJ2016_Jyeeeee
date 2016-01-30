ec4d5718cc519db94b840ae73f1e63877d4c2f7a
Laird <a641713@gmail.com>
2016-01-30 14:36:12 +0800

    no message

--- Assets/Scripts/GameManager_laird.cs
+++ Assets/Scripts/GameManager_laird.cs
@@ -2,12 +2,22 @@
 using System.Collections;
 
 public partial class GameManager : Singleton<GameManager> {
+	public TimeBarView gameTimeBar;
+	float passTime = 0;
+	float remainTime;
+	float gameTimechange;
+	int timer_i;
+
 	private void GameTimeHandle (){
+
+
 		//TODO:reduce time
-//		passTime=Time.time;
-//		timer_i = Mathf.FloorToInt (passTime);
-//		Debug.Log(timer_i);
-//		gameTimeBar.UpdateBar
+		passTime=Time.time;
+		timer_i = Mathf.FloorToInt (passTime);
+		Debug.Log(timer_i);
+		remainTime = 1 - passTime;
+		gameTimeBar.UpdateBar (passTime);
+
 
 	}
 
