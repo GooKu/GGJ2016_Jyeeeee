76b40d13f1d4a9e2c3c6c0fa896401bcf6c009b9
SlimeMeteor <slime.meteor@gamenation.com.tw>
2016-01-30 22:10:56 +0800

    add create mob at runtime, and auto remove when scroll end. add delegate for scroll end.

--- Assets/Scripts/bg_scroll.cs
+++ Assets/Scripts/bg_scroll.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic; // for List.
 
 public class bg_scroll : MonoBehaviour
 {
+	[HideInInspector]
+	public delegate void callbackEventWithGameObject (GameObject go);
+	[HideInInspector]
+	public callbackEventWithGameObject OnScrollEnd = null;
+	
 	private const float scroll_unit = 16.0f;
 
 	public static float m_start_x = 0.0f;
@@ -19,6 +25,11 @@ public class bg_scroll : MonoBehaviour
 	public float m_pos_x = 0.0f; 
 	public float m_pos_y = 0.0f; 
 
+	public GameObject m_goBackgroundSprite;
+	public Component m_comBackgroundSprite;
+
+	public List<GameObject> m_goList_MobInst = new List<GameObject>();
+
 	public void Start ()
 	{
 		Init();
@@ -29,13 +40,52 @@ public class bg_scroll : MonoBehaviour
 
 	public void Init ()
 	{
+		if (m_goList_MobInst == null) {
+			m_goList_MobInst = new List<GameObject>();
+		} else {
+			CleaeAllMobInst();
+		}
+
 		m_scroll_x = true;
 		m_scroll_speed_x = -0.2f;
 		m_start_x = scroll_unit;
 		m_end_x = -(scroll_unit * 2);
 	}
 
-	public void Update ()
+	public void InitEvents ()
+	{
+		OnScrollEnd = (GameObject go) => {
+			Debug.Log ("Default callback : OnScrollEnd()");
+		};
+	}
+
+	public void CreateMobInst (Vector3 mob_pos)
+	{
+		GameObject mob_inst = Instantiate(Resources.Load("Prefab/Mobs/Mob", typeof(GameObject))) as GameObject;
+		
+		if (mob_inst != null) {
+			mob_inst.transform.parent = this.transform;
+			mob_inst.transform.position = mob_pos;
+			m_goList_MobInst.Add(mob_inst);
+			Debug.LogWarning ("CreateMobInst> Add A New Mob Inst!");
+		} else {
+			Debug.LogWarning ("CreateMobInst> Cannot Create Mob!");
+		}
+	}
+	
+	public void CleaeAllMobInst ()
+	{
+		if (m_goList_MobInst == null) {
+			return;
+		}
+
+		foreach (var mob in m_goList_MobInst) {
+			Destroy(mob);
+		}
+		m_goList_MobInst.Clear();
+	}
+
+	public void FixedUpdate ()
 	{
 		m_scroll_x = GameManager.m_bg_scrolling;
 		m_scroll_speed_x = GameManager.m_bg_scroll_speed;
@@ -45,8 +95,15 @@ public class bg_scroll : MonoBehaviour
 		if (m_scroll_x) {
 
 			m_pos_x += m_scroll_speed_x;
+			// end of scroll.
 			if (m_pos_x < m_end_x) {
 				m_pos_x = scroll_unit;
+
+				CleaeAllMobInst();
+
+				if (OnScrollEnd != null) {
+					OnScrollEnd( gameObject );
+				}
 			}
 
 			transform.position = new Vector3(m_pos_x, m_pos_y);
@@ -59,11 +116,27 @@ public class bg_scroll : MonoBehaviour
 
 	}
 
-#if UNITY_EDITOR && SM_DEBUG_SCROLL_BG
+	public GameObject AddChildGameObject(string name)
+	{
+		var go = new GameObject ();
+		go.name = name;
+		go.transform.parent = this.transform;
+		return go;
+	}
+	
+	#if UNITY_EDITOR
 
 	public int m_DebugUI_StartX = 0;
 	public int m_DebugUI_StartY = 0;
+
+#if SM_DEBUG_SCROLL_BG
+		
 	public float m_Debug_Scroll_Speed = -0.05f;
+
+#endif
+
+	private GameObject goMob;
+	private Component comMob;
 
 	public void OnGUI ()
 	{
@@ -72,6 +145,29 @@ public class bg_scroll : MonoBehaviour
 		int yy = m_DebugUI_StartY;
 		int ww = 100;
 		int hh = 20;
+
+#if SM_DEBUG_ADD_MOB
+
+		if (GUI.Button (new Rect (xx, yy, ww, hh), "Add Mob")) {
+			System.Random rnd = new System.Random(System.DateTime.Now.Millisecond);
+
+			GameObject mob_inst = Instantiate(Resources.Load("Prefab/Mobs/Mob", typeof(GameObject))) as GameObject;
+
+			if (mob_inst != null) {
+				mob_inst.transform.parent = this.transform;
+				mob_inst.transform.localPosition = new Vector3((int)((rnd.NextDouble() * 16.0f) - 8.0f), 0, 0);
+				m_goList_MobInst.Add(mob_inst);
+				Debug.Log ("Add A New Mob Inst!");
+			} else {
+				Debug.LogWarning ("Cannot Create Mob!");
+			}
+
+		}
+		yy += hh;
+
+#endif
+
+#if SM_DEBUG_SCROLL_BG
 
 		if (GUI.Button (new Rect (xx, yy, ww, hh), "Speed Down")) {
 			GameManager.Instance.m_bg_scroll_speed += m_Debug_Scroll_Speed;
@@ -87,6 +183,8 @@ public class bg_scroll : MonoBehaviour
 			}
 		}
 		yy += hh;
+
+#endif
 
 	}
 
